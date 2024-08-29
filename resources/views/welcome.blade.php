f6ed9335eb558998779c1f72bdd9b7181a2cdb45
fahmimardiansyah <boilaikskin@gmail.com>
2024-08-29 22:34:26 +0700

    Tugas Biodata Diri

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,19 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>PWL 2024</title>
     </head>
-    <body>
+    {{-- <body>
         <h2>Pemrograman Web Lanjut 2024</h2>
         <h3>Semangat Belajar Laravel 1o</h3>
+    </body> --}}
+    <body>
+        <h1>Pemrograman Web Lanjut 2024</h1>
+        <h2>BIODATA</h2>
+        <br><br>
+        <p>Nama              : Fahmi Mardiansyah</p>
+        <p>NIM               : 2241760064</p>
+        <p>Kelas             : 3A</p>
+        <p>Jurusan           : Teknologi Informasi</p>
+        <p>Tempat, Tgl Lahir : Sumenep, 08 Maret 2004</p>
+        <p>Alamat            : Belimbing, Malang</p>
     </body>
 </html>
